0cf8507aa78418c4b1db414c80f05717d3bc269d
Mrvys <mrvy13@gmail.com>
2018-12-05 17:52:30 +0100

    sending to be

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private http: HttpClient) { }
+
   title = 'wine-recomender';
+  configUrl = 'http://localhost:4200/';
+  sendToServer() {
+    console.log('sending to server...');
+
+    this.sendingService();
+  }
+
+  sendingService() {
+    return this.http.post(this.configUrl, null).subscribe();
+  }
 }
