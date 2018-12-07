d70055b08a99d027a2510699cc1aacffa9401f5b
Mrvys <mrvy13@gmail.com>
2018-12-07 13:05:31 +0100

    sending keywords to server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,27 @@ import { Component } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
 
+export class SendingObject {
+  country: number;
+  keywords: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   constructor(private http: HttpClient) { }
 
   title = 'wine-recomender';
-  configUrl = 'http://localhost:4200/';
+  configUrl = 'http://localhost:8080/api/keywords';
+  data: SendingObject = {
+    country: 1,
+    keywords: null
+  };
+
   sendToServer() {
     console.log('sending to server...');
 
@@ -19,6 +30,6 @@ export class AppComponent {
   }
 
   sendingService() {
-    return this.http.post(this.configUrl, null).subscribe();
+    return this.http.post(this.configUrl, this.data).subscribe();
   }
 }
